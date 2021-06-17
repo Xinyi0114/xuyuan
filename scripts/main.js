17ffafb70545ba1f0214e3af1233bc8366d8b094
yuekaim <yuekaim@gmail.com>
2021-06-18 01:22:50 +0200

    不同browser的scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-
+    // var chrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
     var x1, x2, y1, y2;
 
     x1 = $('#about').position().left;
@@ -8,12 +8,32 @@ $( document ).ready(function() {
     y2 = $('#img2').position().top;
 
     $( "#bt1" ).click(function() {
-      window.scrollTo(x1, y1);
+      if((navigator.userAgent.indexOf("Firefox") != -1)
+          || (navigator.userAgent.indexOf("Chrome") != -1)){
+          window.scrollTo(x1, y1);
+      }else{
+      $("html, body").animate({
+        scrollTop: y1
+      }, {duration: 500, queue: false});
 
-      console.log(x1, y1);
+      $("html, body").animate({
+        scrollLeft: x1
+      }, {duration: 500, queue: false});
+    }
     });
 
     $( "#bt2" ).click(function() {
-      window.scrollTo(x2, y2);
+      if((navigator.userAgent.indexOf("Firefox") != -1)
+          || (navigator.userAgent.indexOf("Chrome") != -1)){
+          window.scrollTo(x2, y2);
+      }else{
+      $("html, body").animate({
+        scrollTop: y2
+      }, {duration: 500, queue: false});
+
+      $("html, body").animate({
+        scrollLeft: x2
+      }, {duration: 500, queue: false});
+    }
     });
 });
