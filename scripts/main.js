b2400c604ac12008de60e78b79c3916d4e933797
yuekaim <yuekaim@gmail.com>
2021-06-17 21:37:02 +0200

    css

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
     var x1, x2, y1, y2;
 
     x1 = $('#img1').position().left;
+    console.log(x1);
     y1 = $('#img1').position().top;
     x2 = $('#img2').position().left;
     y2 = $('#img2').position().top;
