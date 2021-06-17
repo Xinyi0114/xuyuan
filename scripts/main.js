075da8851e6b421ccf1ddec5946709218d76840b
yuekaim <yuekaim@gmail.com>
2021-06-17 21:57:37 +0200

    css menu

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,17 @@ $( document ).ready(function() {
     var x1, x2, y1, y2;
 
     x1 = $('#img1').position().left;
-    console.log(x1);
     y1 = $('#img1').position().top;
     x2 = $('#img2').position().left;
     y2 = $('#img2').position().top;
 
     $( "#bt1" ).click(function() {
-      $(window).scrollTo(x1, y1);
+      window.scrollTo(x1, y1);
+
+      console.log(x1, y1);
     });
 
     $( "#bt2" ).click(function() {
-      $(window).scrollTo(x2, y2);
+      window.scrollTo(x2, y2);
     });
 });
