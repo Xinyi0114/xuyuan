250ab61172528e18ad69526e3b7ed2d5d39adc4a
yuekaim <yuekaim@gmail.com>
2021-06-17 21:29:00 +0200

    main js scrollTo

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+    var x1, x2, y1, y2;
+
+    x1 = $('#img1').position().left;
+    y1 = $('#img1').position().top;
+    x2 = $('#img2').position().left;
+    y2 = $('#img2').position().top;
+
+    $( "#bt1" ).click(function() {
+      $(window).scrollTo(x1, y1);
+    });
+
+    $( "#bt2" ).click(function() {
+      $(window).scrollTo(x2, y2);
+    });
+});
